e346b03be4e5b35501284d9ca0f114481643e0a4
javiab10 <javierbasoko@gmail.com>
2024-11-10 19:26:26 +0100

    Comentarios, correcciones y control de errores
    - Se han añadido comentarios en el index.html y en el script.js para explicar mejor el código.
    
    - Se ha corregido algún elemento cuyo nombre estaba mal.
    
    - Se han quitado los console.log que se usaban para comprobar el funcionameiento de algunos métodos.
    
    - Se ha agregado el control de un error para que se genere una alerta cuando el usuario no introduzca ninguna ciudad en la sección "Buscar".

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,33 @@
 jQuery(document).ready(function () {
+    //ApiKey y Url
     const apiKey = "2c3d63eb764dc73a9dbd4e6dc6e4d756";
     var url;
-    btnMainBuscar = $("#btnMainBuscar");
-    btnMainPronostico = $("#btnPronostico");
-    btnMainUbi = $("#btnMainUbi");
-    btnMainPronosticoUbi = $("#btnPronosticoUbi");
+
+    //Partes de la página
     menu = $("#offcanvasNavbar");
-    
     inicio = $("#inicio");
     buscar = $("#buscar");
     ubi = $("#ubi");
     pronostico = $("#pronostico");
     pronosticoUbi = $("#pronosticoUbi");
     
+    //Botones
+    btnMainBuscar = $("#btnMainBuscar");
+    btnMainPronostico = $("#btnPronostico");
+    btnMainUbi = $("#btnMainUbi");
+    btnMainPronosticoUbi = $("#btnPronosticoUbi");
     btnMenuInicio = $("#btnMenuInicio");
     btnMenuBuscar = $("#btnMenuBuscar");
     btnMenuUbi = $("#btnMenuUbi");
 
+    //Imagenes
     imgClaro = "./assets/img/claro.png";
     imgNuboso = "./assets/img/nuboso.png";
     imgLluvia = "./assets/img/lluvia.png";
     imgTormenta = "./assets/img/tormenta.png";
     imgNieve = "./assets/img/nieve.png";
 
+    //Mostrar únicamente Inicio al comenzar
     buscar.hide();
     ubi.hide();
     pronostico.hide();
@@ -72,6 +77,7 @@ jQuery(document).ready(function () {
         $("#btnClose").click();
     }); 
 
+    //Botón para mostrar el tiempo en tu ubicación
     btnMainUbi.on("click", function () {
         if("geolocation" in navigator){
             navigator.geolocation.getCurrentPosition(function(position){
@@ -125,8 +131,6 @@ jQuery(document).ready(function () {
                 url: url,
                 dataType: "json",
                 success: function (pronosticos) {
-                    console.log(pronosticos);
-                    console.log(pronosticos.name);
                     //Para cada array pronosticos.list busca aquellos cuyo atributo dt_text contenga el texto "12:00:00"
                     var pronosticos12 = pronosticos.list.filter(function (pronostico){
                         return pronostico.dt_txt.includes("12:00:00");
@@ -164,44 +168,46 @@ jQuery(document).ready(function () {
 
     //Botón para buscar el tiempo en la ciudad introducida en el formulario "ciudad"
     btnMainBuscar.on("click", function () {
-        pronostico.show();
+        
         $("#cardContainer").empty();
         $("#pronosticoContainer").empty();
         var ciudad = $("#ciudad").val();
         url = "https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&appid="+apiKey+"&units=metric";
-        
-        $.ajax({
-            type: "GET",
-            url: url,
-            dataType: "json",
-            success: function (ciudad) {
-                console.log(ciudad);
-                console.log(ciudad.weather);
-                var img;
-                if(ciudad.weather[0].main === "Clear"){
-                    img = imgClaro;                    
-                }
-                else if(ciudad.weather[0].main === "Clouds"){
-                    img = imgNuboso;                    
-                }
-                else if(ciudad.weather[0].main === "Rain"){
-                    img = imgLluvia;
-                }
-                else if(ciudad.weather[0].main === "Thunderstorm"){
-                    img = imgTormenta;
-                }
-                else if(ciudad.weather[0].main === "Snow"){
-                    img = imgNieve;
-                }else{
-                    img = imgClaro;
-                }
 
-                var card = '<div class="col-lg-3 col-md-4 col-sm-6 mb-4"><div class="card h-100 pt-3 pb-3"><h4 class="card-text text-center">'+ciudad.name+'</h4><div class="card-body d-flex justify-content-center"><img class="img-fluid" src="'+img+'"alt="Card image cap"></div><p class="card-text text-center">'+ciudad.main.temp+'ºC</p></div>';
-
-                $("#cardContainer").append(card);
-                
-            }
-        });
+        if(ciudad === ""){
+            alert("Ninguna ciudad introducida");
+        }else{
+            pronostico.show();
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: "json",
+                success: function (ciudad) {
+                    var img;
+                    if(ciudad.weather[0].main === "Clear"){
+                        img = imgClaro;                    
+                    }
+                    else if(ciudad.weather[0].main === "Clouds"){
+                        img = imgNuboso;                    
+                    }
+                    else if(ciudad.weather[0].main === "Rain"){
+                        img = imgLluvia;
+                    }
+                    else if(ciudad.weather[0].main === "Thunderstorm"){
+                        img = imgTormenta;
+                    }
+                    else if(ciudad.weather[0].main === "Snow"){
+                        img = imgNieve;
+                    }else{
+                        img = imgClaro;
+                    }
+    
+                    var card = '<div class="col-lg-3 col-md-4 col-sm-6 mb-4"><div class="card h-100 pt-3 pb-3"><h4 class="card-text text-center">'+ciudad.name+'</h4><div class="card-body d-flex justify-content-center"><img class="img-fluid" src="'+img+'"alt="Card image cap"></div><p class="card-text text-center">'+ciudad.main.temp+'ºC</p></div>';
+    
+                    $("#cardContainer").append(card);
+                }
+            });
+        }
     });
 
     //Botón para pronosticar los 5 días siguientes en la ciudad que hayas buscado
